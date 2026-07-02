a63c474b80a3497c0fa932a90a29424e9209c638
jonesernesto16-cloud <jonesernesto16@gmail.com>
2026-07-02 19:38:13 +0200

    Adding style to scrapping

--- mini-project/web/script.js
+++ mini-project/web/script.js
@@ -1,24 +1,27 @@
 fetch("data/product.json")
-.then(response => response.json())
-.then(data => {
-    // console.log(data);
-    // data.forEach(product => {
-        let i = 0;
-        ob = Object.keys(data);
-        for (i = 0; i < ob.length; i++) { 
-            console.log(i);
-            let product = data[i];
+    .then(response => response.json())
+    .then(data => {
+        const productList = document.querySelector("#product-list");
+        
+        Object.values(data).forEach(product => {
             const productElement = document.createElement("div");
             productElement.classList.add("product");
+            
+            const tagsHTML = product.categories_tags
+                .map(tag => `<li class="tag">${tag}</li>`)
+                .join('');
+            
             productElement.innerHTML = `
-                <h2>${product.product_name}</h2>
-                <p>Brand: ${product.brands}</p>
-                <p>Ingredients: ${product.ingredients_text}</p>
                 <img src="${product.image_url}" alt="${product.product_name}">
-                <p>Categories: ${product.categories_tags}</p>
-
+                <h2>${product.product_name}</h2>
+                <p>${product.brands}</p>
+                <p>${product.ingredients_text}</p>
+                <ul>
+                    ${tagsHTML}
+                </ul>
             `;
-            document.body.appendChild(productElement);
-        }
-    
-})
+            
+            productList.appendChild(productElement);
+        });
+    })
+    .catch(error => console.error('Error loading products:', error));
